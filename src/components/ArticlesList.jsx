548ba948b2647b0858b1fc45d52bb37869f29026
Abigail Lee <226848368+abigailys@users.noreply.github.com>
2026-02-24 11:57:01 +0000

    add loading status and error handling to articles page

--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -4,25 +4,38 @@ import ArticleCard from "./ArticleCard";
 
 function ArticlesList() {
     const [articles, setArticles] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState();
 
     useEffect(() => {
-       async function fetchArticles() {
-        const articlesResult = await getArticles()
-        setArticles(articlesResult);
-       } 
-
-       fetchArticles()
+        async function fetchArticles() {
+            try {
+                setIsLoading(true)
+                const articlesResult = await getArticles()
+                setArticles(articlesResult);
+            } catch (err) {
+                setError(err.message || "Something went wrong!");
+            } finally {
+                setIsLoading(false);
+            }
+}
+            fetchArticles()
+        
     }, [])
-    
+
+    if (error) {
+    return <div>Error: {error}</div>;
+  }
     return (
         <>
-        <div className="articles-list">
-            {articles.map((article) => (
-                <ArticleCard key={article.article_id} articleObject={article}/>
-            ))}
-        </div>
+            <div>{isLoading && "Loading..."}</div>
+            <div className="articles-list">
+                {articles.map((article) => (
+                    <ArticleCard key={article.article_id} articleObject={article} />
+                ))}
+            </div>
         </>
-        )
+    )
 }
 
 export default ArticlesList;
