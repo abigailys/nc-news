f1e70edc914a0ef13d2371551c8798441a980270
Abigail Lee <226848368+abigailys@users.noreply.github.com>
2026-02-25 16:12:33 +0000

    add user context

---
+++ src/context/User.jsx
@@ -0,0 +1,18 @@
+
+import { useState } from 'react'
+import { createContext } from "react";
+
+export const UserContext = createContext();
+
+function UserProvider({children}) {
+    const [currentUser, setCurrentUser] = useState({
+            username: 'grumpy19',
+            name: 'Paul Grump',
+            avatar_url:
+                'https://vignette.wikia.nocookie.net/mrmen/images/7/78/Mr-Grumpy-3A.PNG/revision/latest?cb=20170707233013'
+        })
+
+    return <UserContext value={{currentUser}}>{children}</UserContext>
+}
+
+export default UserProvider;
