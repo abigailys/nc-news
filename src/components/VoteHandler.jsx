dbeb94a758e90812308312bb6a6c8e95018a20f5
Abigail Lee <226848368+abigailys@users.noreply.github.com>
2026-02-26 20:56:51 +0000

    add error handling for failed vote action

--- src/components/VoteHandler.jsx
+++ src/components/VoteHandler.jsx
@@ -1,18 +1,23 @@
+import { useState } from "react";
+
 function VoteHandler({ itemId, setVotes, updateArticleVotes }) {
 
+    const [error, setError] = useState(null);
 
     async function updateVotes(inc_votes) {
         setVotes((votes) => votes + inc_votes) // optimistic render
-
+        setError(null);
         try {
             await updateArticleVotes(itemId, inc_votes) // update database
         } catch (error) {
             setVotes((votes) => votes - inc_votes) // reverse if failed
+            setError("Failed to vote. Please try again.");
         }
     }
 
     return (
         <>
+            {error && <p>{error}</p>}
             <button onClick={() => { updateVotes(1) }}>Upvote</button>
             <button onClick={() => { updateVotes(-1) }}>Downvote</button>
         </>
